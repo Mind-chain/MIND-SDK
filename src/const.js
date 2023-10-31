c8782ff52200801999820380fe2a9e0adc16a712
Your Name <you@example.com>
2023-10-31 09:40:07 +0600

    init

---
+++ src/const.js
@@ -0,0 +1,12 @@
+export const urls = {
+    http: "https://seednode.mindchain.info/",
+    ws: "wss://seednode.mindchain.info/ws",
+    explorer_api: "https://mainnet.mindscan.info/api/v2/",
+  };
+  
+  export const contracts = {
+    STAKING: "0x0000000000000000000000000000000000001001",
+    PERRY_MIND: "0x75E218790B76654A5EdA1D0797B46cBC709136b0",
+    MULTICALL: "0x7B1f198bE2Dd0884a2d8A459f4F71bf557E73D8A",
+  };
+  
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { urls, contracts } from "./const.js";
+
+const url = urls;
+const contract = contracts;
+
+export { url, contract };
